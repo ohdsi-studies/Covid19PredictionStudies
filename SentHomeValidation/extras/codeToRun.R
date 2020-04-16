a459eeedca7671cc9035cb5f4e5c9fe8decb09da
jreps <jreps@its.jnj.com>
2020-04-16 10:57:32 -0400

    adding note about fftempdir

--- SentHomeValidation/extras/codeToRun.R
+++ SentHomeValidation/extras/codeToRun.R
@@ -22,6 +22,7 @@ outputFolder <- './SentHomeValidation'
 minCellCount <- 10
 
 # add connection details:
+# NOTE: make sure the folder you set for fftempdir exists or you will get an error
 options(fftempdir = 'T:/fftemp')
 dbms <- "pdw"
 user <- NULL
