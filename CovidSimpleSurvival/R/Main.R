3d9fa16aeb878ca5bb94d9c18cffff10b6dac376
jreps <jreps@its.jnj.com>
2020-04-20 13:51:27 -0400

    Update Main.R
    changing validationResults.rds to validationResult.rds

--- CovidSimpleSurvival/R/Main.R
+++ CovidSimpleSurvival/R/Main.R
@@ -208,8 +208,8 @@ execute <- function(connectionDetails,
           dir.create(file.path(outputFolder,cdmDatabaseName,paste0('Analysis_',sa$analysisId)), recursive = T)
         }
         ParallelLogger::logInfo("Saving results")
-        saveRDS(result, file.path(outputFolder,cdmDatabaseName,paste0('Analysis_',sa$analysisId),'validationResults.rds'))
-        ParallelLogger::logInfo(paste0("Results saved to:",file.path(outputFolder,cdmDatabaseName,paste0('Analysis_',sa$analysisId),'validationResults.rds')))
+        saveRDS(result, file.path(outputFolder,cdmDatabaseName,paste0('Analysis_',sa$analysisId),'validationResult.rds'))
+        ParallelLogger::logInfo(paste0("Results saved to:",file.path(outputFolder,cdmDatabaseName,paste0('Analysis_',sa$analysisId),'validationResult.rds')))
         
       }
     }
